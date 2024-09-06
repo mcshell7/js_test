ef66f06e7305b8b9669e83633106639b257c4797
mcshell7 <mishakhamardiuk@gmail.com>
2024-09-06 14:31:01 -0400

    project updated and optimized

--- main.js
+++ main.js
@@ -5,10 +5,9 @@ let fileNameDetails = 'user-details.html';
 let fileNamePost = 'post-details.html';
 
 
-let usersList = document.getElementById('users');
-
-function createElement(element, className, content) {
+function createElement(element, className, content, attributes = [{}]) {
     const elem = document.createElement(element);
+
     if (className) {
         if (Array.isArray(className)) {
             for (let i = 0; i < className.length; i++) {
@@ -18,114 +17,117 @@ function createElement(element, className, content) {
             elem.classList.add(className);
         }
     }
+
     if (content) {
         elem.textContent = content;
     }
+
+    if (attributes){
+        for (let attr of attributes) {
+            for (const attrKey in attr) {
+                elem.setAttribute(attrKey, attr[attrKey]);
+            }
+        }
+    }
+
+
     return elem;
 }
 
-
-async function usersFetch() {
-    let response = await fetch(url).then(value => value.json());
-    for (let i = 0; i < response.length; i++) {
-        displayUsers(response[i]);
-    }
-}
-
-let displayUsers = (user) => {
-    const card = createElement('li', ['card', 'text-center']);
-    const cardBody = createElement('div', 'card-body');
-
-    const name = createElement('p', 'card-text', user.name);
-    const id = createElement('h2', 'card-title', user.id);
-    const btn = createElement('a', ['btn', 'btn-sm', 'btn-primary'], 'Show more');
-
-
-    btn.href = fileNameDetails;
-
-    card.appendChild(cardBody);
-    cardBody.appendChild(id);
-    cardBody.appendChild(name);
-    cardBody.appendChild(btn);
-    if (usersList) {
-        usersList.appendChild(card);
-    }
-    btn.addEventListener('click', (e) => {
-        e.preventDefault();
-        window.location.href = fileNameDetails;
-        localStorage.setItem('user', JSON.stringify(user));
-    });
-}
 if (fileName === 'index.html') {
+    async function usersFetch() {
+        try {
+            const response = await fetch(url);
+            const data = await response.json();
+            data.forEach(user => displayUsers(user));
+        } catch (error) {
+            console.error('Error fetching users:', error);
+        }
+    }
+
+    const usersList = createElement('ul', ['user__list', 'text-center'], 0, [{id: 'users'}]);
+    let displayUsers = (user) => {
+
+        const card = createElement('li', ['card', 'text-center'], 0);
+        const cardBody = createElement('div', 'card-body', 0);
+        const name = createElement('p', 'card-text', user.name);
+        const id = createElement('h2', 'card-title', user.id);
+        const btn = createElement('a', ['btn', 'btn-sm', 'btn-primary'], 'Show more');
+
+        document.body.append(usersList);
+        card.append(cardBody);
+        cardBody.append(id, name, btn);
+        usersList.append(card);
+
+        btn.addEventListener('click', (e) => {
+            e.preventDefault();
+            window.location.href = fileNameDetails;
+            localStorage.setItem('user', JSON.stringify(user));
+        });
+    }
     usersFetch();
 }
+
 if (fileName === 'user-details.html') {
-    const storedUser = JSON.parse(localStorage.getItem('user'));
-    let displayUsersFull = () => {
-        let userBlock = document.getElementById('user');
+    let displayUsersFull = (storedUser) => {
+
+        let userBlock = createElement('ul', ['info', 'list-group'], 0, [{id: 'user'}]);
+        document.body.append(userBlock);
+
         for (const eKey in storedUser) {
-            let item = createElement('li', [`info__${eKey}`, 'list-group-item']);
+            let item = createElement('li', [`info__${eKey}`, 'list-group-item'], 0);
             userBlock.appendChild(item);
             if (typeof storedUser[eKey] !== 'object') {
                 item.textContent = storedUser[eKey];
             } else {
                 item.textContent = eKey.charAt(0).toUpperCase() + eKey.slice(1) + ':';
-                let subList = createElement('ul', 'sub__list');
+                const subList = createElement('ul', 'sub__list', 0);
                 let storedUserSub = storedUser[eKey];
+
                 for (const key in storedUserSub) {
                     let subItem = createElement('li', 'sub__item', storedUser[eKey][key]);
                     if (typeof storedUserSub[key] !== 'object') {
                         subList.appendChild(subItem);
                     } else if (typeof storedUserSub[key] === 'object') {
-                        let subList2 = createElement('ul', 'sub__list');
-                        let subItem1 = createElement('li', 'sub__list1', storedUserSub[key].lat);
-                        let subItem2 = createElement('li', 'sub__list1', storedUserSub[key].lng);
-                        subList2.appendChild(subItem1);
-                        subList2.appendChild(subItem2);
+                        const subList2 = createElement('ul', 'sub__list', 0);
+                        for (const subItemKey in storedUserSub[key]) {
+                            const subItem1 = createElement('li', 'sub__list1', storedUserSub[key][subItemKey]);
+                            subList2.appendChild(subItem1);
+                        }
                         subList.appendChild(subList2);
                     }
                 }
                 item.appendChild(subList);
             }
         }
-        let btn = createElement('a', ['post__btn', 'btn', 'btn-sm', 'btn-primary']);
-        let btnContainer = createElement('div', ['posts__btn-container']);
-        btn.textContent = 'posts of current user';
-        btn.setAttribute("id", "post__btn");
+        const btn = createElement('a', ['post__btn', 'btn', 'btn-sm', 'btn-primary'], 'posts of current user', [{id: 'post__btn'}]);
+        const btnContainer = createElement('div', ['posts__btn-container'], 0);
+
         document.body.append(btnContainer);
         btnContainer.append(btn);
     }
-    displayUsersFull();
+
+    const getUser = JSON.parse(localStorage.getItem('user'));
+    displayUsersFull(getUser);
 }
 if (fileName === fileNameDetails) {
-    let userId = JSON.parse(localStorage.getItem('user')).id;
-    let urlPosts = `https://jsonplaceholder.typicode.com/users/${userId}/posts`;
-
-    async function fetchPosts() {
-        await fetch(urlPosts)
-            .then(value => value.json())
-            .then(posts => addPostsToStorage(posts));
-    }
 
     function addPostsToStorage(posts) {
-        let btn = document.getElementById('post__btn');
+        const btn = document.getElementById('post__btn');
 
         function handleClickAllPosts(e) {
             e.preventDefault();
 
             // adding all posts to Storage by this user ID
             localStorage.setItem('posts', JSON.stringify(posts));
-            const postList = createElement('ul', ['posts']);
+            const postList = createElement('ul', ['posts'], 0);
 
             for (const item of posts) {
 
-                const postItem = createElement('li', 'card');
-                const postContainer = createElement('li', 'card-body');
-                const postTitle = createElement('h4', 'h4');
-                const postBtn = createElement('a', ['full-post', 'btn', 'btn-primary', 'btn-sm']);
-
-                postTitle.textContent = item.title;
-                postBtn.textContent = 'Show more';
+                const postItem = createElement('li', 'card', 0);
+                const postContainer = createElement('li', 'card-body', 0);
+                const postTitle = createElement('h4', 'h4', item.title);
+                const postBtn = createElement('a', ['full-post', 'btn', 'btn-primary', 'btn-sm'], 'Show more');
 
                 postItem.append(postContainer);
                 postContainer.append(postTitle, postBtn);
@@ -138,69 +140,71 @@ if (fileName === fileNameDetails) {
             }
             // adding list with all posts by user ID
             document.body.append(postList);
-
             // removing event listener after 1 click
             btn.removeEventListener('click', handleClickAllPosts);
         }
-
         btn.addEventListener('click', handleClickAllPosts);
     }
 
+    async function fetchPosts(url) {
+        await fetch(url)
+            .then(value => value.json())
+            .then(posts => addPostsToStorage(posts))
+    }
 
-    fetchPosts();
+    const userId = JSON.parse(localStorage.getItem('user')).id;
+    const urlPosts = `https://jsonplaceholder.typicode.com/users/${userId}/posts`;
+
+    fetchPosts(urlPosts);
 }
 
-function getPostsFromStorage() {
-    const storedPost = JSON.parse(localStorage.getItem('post'));
-    console.log(storedPost);
-    let postContainer = createElement('div', ['post']);
-    document.body.append(postContainer);
-
-    let id = createElement('div', ['id']);
-    let title = createElement('h1', ['title', 'h1']);
-    let content = createElement('div', ['content']);
-    id.textContent = 'post ID: ' + storedPost.id;
-    title.textContent = storedPost.title;
-    content.textContent = storedPost.body;
-
-    postContainer.append(title, content, id);
-    return storedPost;
-}
 
 if (fileName === fileNamePost) {
-    let postId = getPostsFromStorage().id;
-    let urlComments = `https://jsonplaceholder.typicode.com/posts/${postId}/comments`;
+
+    function getPostsFromStorage() {
+        const storedPost = JSON.parse(localStorage.getItem('post'));
+
+        const postContainer = createElement('div', ['post'], 0);
+        const id = createElement('div', ['id'], `post ID: ${storedPost.id}`);
+        const title = createElement('h1', ['title', 'h1'], storedPost.title);
+        const content = createElement('div', ['content'], storedPost.body);
+
+        document.body.append(postContainer);
+        postContainer.append(title, content, id);
+        return storedPost;
+    }
+
     function showAllComments(comments) {
-        let commentsBlock = createElement('div', ['comments']);
-        let commentsList = createElement('div', ['comments__list']);
-        let commentsHeading = createElement('h2', 'h2','Comments');
+        const commentsBlock = createElement('div', ['comments'], 0);
+        const commentsList = createElement('ul', ['comments__list'], 0);
+        const commentsHeading = createElement('h2', 'h2', 'Comments');
         commentsBlock.append(commentsHeading, commentsList);
         document.body.append(commentsBlock);
-        console.log(comments);
 
         for (const key in comments) {
-            let comment = createElement('div', ['comment', 'card']);
-            let commentContainer = createElement('div', ['comment', 'card-body']);
-            let commentContent = createElement('div', ['comment__content'], comments[key].body);
-            let commentId = createElement('div', ['comment__id'],`id: ${comments[key].id}`);
-            let commentEmail = createElement('a', ['comment__email'],comments[key].email);
-            commentEmail.href = `mailto:${comments[key].email}`;
-            let commentName = createElement('h4', ['comment__name', 'h4'],comments[key].name);
-            let commentPostId = createElement('div', ['comment__post-id'],`Post ID: ${comments[key].postId}`);
+            const comment = createElement('li', ['comment', 'card']);
+            const commentContainer = createElement('div', ['comment', 'card-body']);
+            const commentContent = createElement('div', ['comment__content'], comments[key].body);
+            const commentId = createElement('div', ['comment__id'], `id: ${comments[key].id}`);
+            const commentEmail = createElement('a', ['comment__email'], comments[key].email, [{href: `mailto:${comments[key].email}`}]);
+            const commentName = createElement('h4', ['comment__name', 'h4'], comments[key].name);
+            const commentPostId = createElement('div', ['comment__post-id'], `Post ID: ${comments[key].postId}`);
+
             commentsList.append(comment);
             comment.append(commentContainer);
-            commentContainer.append(commentName,commentContent,commentId,commentPostId,commentEmail);
-
+            commentContainer.append(commentName, commentContent, commentId, commentPostId, commentEmail);
         }
     }
 
-    async function fetchComments() {
-        let response = await fetch(urlComments)
-            .then(value => value.json())
-            .then(comments => showAllComments(comments));
+    async function fetchComments(postId) {
+        let urlComments = `https://jsonplaceholder.typicode.com/posts/${postId}/comments`;
+
+        await fetch(urlComments)
+            .then(response => response.json())
+            .then(response => {
+                showAllComments(response);
+            })
     }
-    fetchComments();
-
+    let postId = getPostsFromStorage().id;
+    fetchComments(postId);
 }
-
-
